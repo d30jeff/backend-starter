aff5d416b51e0caa66a3ea5a919bdd1af6f5e249
Deojeff Lai <deojeff.lai@petronas.com>
2024-07-01 15:12:14 +0800

    Remove default module implementation

--- src/servers/admin/admin.controllers.ts
+++ src/servers/admin/admin.controllers.ts
@@ -1,9 +1,4 @@
 import { AdminController } from '@/servers/admin/admin/admin.controller.js';
-import { CountryController } from '@/servers/admin/country/country.controller.js';
 import { HealthcheckController } from '@/servers/admin/healthcheck/healthcheck.controller.js';
 
-export const controllers = [
-  AdminController,
-  CountryController,
-  HealthcheckController,
-];
+export const controllers = [AdminController, HealthcheckController];
